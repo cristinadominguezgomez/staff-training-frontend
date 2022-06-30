b3847aa139d1db6ee8ad6e1aa46624eb90e7e2db
Lilen Campodonico <campodonicolilen1@gmail.com>
2022-06-30 13:00:30 +0200

    login form

---
+++ src/services/auth/loginService.js
@@ -0,0 +1,17 @@
+export const loginService = async ({ email, password }) => {
+  const response = await fetch(`${process.env.REACT_APP_BACKEND}/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  });
+
+  const json = await response.json();
+
+  if (!response.ok) {
+    throw new Error(json.message);
+  }
+
+  return json.token;
+};
