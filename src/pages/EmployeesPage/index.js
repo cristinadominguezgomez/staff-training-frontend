1baceaa37a8d3ff18178db32b45a961b7b7f31ec
cristina <dominguez.ac@gmail.com>
2022-07-07 16:22:05 +0200

    fix: conditional rendering admin

--- src/pages/EmployeesPage/index.js
+++ src/pages/EmployeesPage/index.js
@@ -1,15 +1,17 @@
 import { EmployeesList } from "../../components/EmployeesList";
-
+import { EmployeeTokenContext } from "../../context/EmployeeTokenContext";
 import useEmployees from "../../hooks/useEmployees";
+import { useContext } from "react";
 
 export const EmployeesPage = () => {
   const { employees, loading, error, removeEmployee } = useEmployees();
+  const { employee: employeeContext } = useContext(EmployeeTokenContext);
 
   if (loading) return <p>Cargando employees...</p>;
 
   if (error) return <p>{error}</p>;
 
-  return employees ? (
+  return employeeContext && employeeContext.role === "admin" ? (
     <section>
       <h1>Employees Page</h1>
 
