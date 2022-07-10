171161218f3cfe96a5032dfb2e24327a7629c78a
Lilen Campodonico <campodonicolilen1@gmail.com>
2022-07-10 12:52:35 +0200

    style: table home

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,3 +1,4 @@
+import "../HomePage/style.css";
 import { useEmployeeTokenContext } from "../../context/EmployeeTokenContext";
 
 export const HomePage = () => {
@@ -14,16 +15,61 @@ export const HomePage = () => {
         <p>landing page para usuario normal</p>
       ) : (
         <>
-          <p>landing page para personas no logueadas</p>
-          <h2>Home Page</h2>
-          <p>Welcome to staff training. </p>
-          <p>
-            If you are a member of our work team, you can register and log in to
-            see all the exercises that are available on our page, to help you
-            plan your classes and training.
-          </p>
+          <div className="home">
+            <div className="message">
+              <h2>Home Page</h2>
+              <h4>Welcome to staff training. </h4>
+              <p>
+                If you are a member of our work team, you can register and log
+                in to see all the exercises that are available on our page, to
+                help you plan your classes and training.
+              </p>
 
-          <p> Thank you!</p>
+              <p> Thank you!</p>
+            </div>
+
+            <div className="table">
+              <h2>Horario de Apertura</h2>
+              <table>
+                <thead>
+                  <tr>
+                    <th>Day</th>
+                    <th>Hour</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Monday</td>
+                    <td>7 a.m. / 11 p.m.</td>
+                  </tr>
+                  <tr>
+                    <td>Tuesday</td>
+                    <td>8 a.m. / 12 p.m.</td>
+                  </tr>
+                  <tr>
+                    <td>Wednesday</td>
+                    <td>7 a.m. / 11 p.m.</td>
+                  </tr>
+                  <tr>
+                    <td>Thursday</td>
+                    <td>8 a.m. / 12 p.m.</td>
+                  </tr>
+                  <tr>
+                    <td>Friday</td>
+                    <td>7 a.m. / 11 p.m.</td>
+                  </tr>
+                  <tr>
+                    <td>Saturday</td>
+                    <td>7 a.m. / 3 p.m.</td>
+                  </tr>
+                  <tr>
+                    <td>Sunday</td>
+                    <td>CLOSED</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
         </>
       )}
     </>
