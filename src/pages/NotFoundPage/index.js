1a76bba02fe0547d8d131e815b1804cfd07f3476
Shirley Gianina <shirley.cd853f@Shirleys-MacBook-Pro.local>
2022-07-10 23:44:18 +0200

    style: page not found

--- src/pages/NotFoundPage/index.js
+++ src/pages/NotFoundPage/index.js
@@ -1,9 +1,26 @@
 import { Link } from "react-router-dom";
+import "./style.css"
 export const NotFoundPage = () => {
   return (
-    <section>
-      <h2>Not found</h2>
-      <Link to={"/"}>Go to Home Page</Link>
+    // https://codepen.io/sqfreakz/pen/GJRJOY
+    <section className="not-found">
+      <div id="clouds">
+        <div className="cloud x1"></div>
+        <div className="cloud x1_5"></div>
+        <div className="cloud x2"></div>
+        <div className="cloud x3"></div>
+        <div className="cloud x4"></div>
+        <div className="cloud x5"></div>
+      </div>
+      <div className="c">
+        <div className="_404">404</div>
+        <hr />
+        <div className="_1">THE PAGE</div>
+        <div className="_2">WAS NOT FOUND</div>
+        <Link className="btn" to={"/"}>
+          BACK TO MARS
+        </Link>
+      </div>
     </section>
   );
 };
